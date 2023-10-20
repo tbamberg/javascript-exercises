ae61596ccddc6e047ef1306b0222413ff482d370
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-10-20 15:57:08 -0700

    Initial commit, add code to satisfy first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,20 @@
-const reverseString = function() {
+/* pseudocode
+create "answer"
+.split string into an array
+.reverse array in place
+.join array into string
+return string
+*/
 
+const reverseString = function(string) {
+    let answer = "";
+    let array = [];
+
+    array = string.split("");
+    array = array.reverse();
+    answer = array.join("");
+
+    return answer;
 };
 
 // Do not edit below this line
