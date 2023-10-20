b6540cf5e8d8d848453932af619a1d30350851d0
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-10-20 15:44:20 -0700

    Add error handling for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+
     let answer = '';
+
     for (let i = 0; i < num; i++) {
         answer = answer.concat(string);
     }
