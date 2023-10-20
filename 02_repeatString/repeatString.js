f8b8c0d279456aae8e32f21ee4aa3daab7ae956a
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-10-20 15:42:17 -0700

    Add code to match first test, add more tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let answer = '';
+    for (let i = 0; i < num; i++) {
+        answer = answer.concat(string);
+    }
 
+    return answer;
 };
 
 // Do not edit below this line
