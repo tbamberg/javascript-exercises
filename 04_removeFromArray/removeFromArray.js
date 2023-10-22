d5ec688cc342f1cb77e2d3482ce7e89593832f68
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-10-21 23:10:29 -0700

    Add initial pseudocode/logic

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+/* pseudocode
+function takes an array and unknown number of additional arguments
+for each argument after array
+    .find argument in array
+    if found, .splice array and remove found arg
+return spliced array
+*/
+
+const removeFromArray = function(givenArray, ...arguments) {
+    
 
 };
 
