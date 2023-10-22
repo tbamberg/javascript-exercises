45bdce30abf4a4a1bb20666ba77e0554a062feb4
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-10-22 00:39:29 -0700

    Add error handling for non-positive ints

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -8,6 +8,14 @@ return sum
 */
 
 const sumAll = function(min, max) {
+    if (min < 0 || max < 0) return "ERROR";
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min > max) {
+        let tempMin = min;
+        min = max;
+        max = tempMin;
+    }
+
     let sum = 0;
 
     for (min; min <= max; min++) {
