48ba4173023f4c297a7861e1841fffbb79613034
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-10-21 23:49:36 -0700

    Create function to fulfill README requirements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,20 @@
 /* pseudocode
 function takes an array and unknown number of additional arguments
 for each argument after array
-    .find argument in array
+    .indexOf argument in array
     if found, .splice array and remove found arg
 return spliced array
 */
 
-const removeFromArray = function(givenArray, ...arguments) {
-    
-
+const removeFromArray = function(givenArray, ...otherArguments) {
+    for (arg of otherArguments) {
+        let index = givenArray.indexOf(arg);
+        givenArray.splice(index,1);
+    }
+    return givenArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
