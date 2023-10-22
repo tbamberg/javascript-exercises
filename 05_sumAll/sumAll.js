4fafa90471647b1e4da11c3ff3f1eb18923e9ae1
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-10-22 00:25:22 -0700

    Add code to meet README reqs, pass first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+/* pseudocode
+create variable sum = 0
+check if args are positive integers
+    if not, return "ERROR"
+if min > max, swap min and max
+for each int in set, add to sum
+return sum
+*/
 
+const sumAll = function(min, max) {
+    let sum = 0;
+
+    for (min; min <= max; min++) {
+        sum += min;
+    }
+    return sum;
 };
 
 // Do not edit below this line
