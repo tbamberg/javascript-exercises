f8f043d2a49dae1330cb9719801d1a83cb10386b
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-11-09 16:17:09 -0800

    Add functionality for remaining functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -2,24 +2,30 @@ const add = function(a, b) {
 	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = array.reduce((subTotal, a) => subTotal + a, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = array.reduce((subTotal, a) => subTotal * a, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for (let i = 2; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
